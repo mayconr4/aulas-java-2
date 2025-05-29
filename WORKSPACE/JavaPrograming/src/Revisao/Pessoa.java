ca12047dea188984764d661a295d08961915dafd
mayconr4 <mayconhs2007@gmail.com>
2025-05-29 14:44:17 -0300

    primeira classe

---
+++ WORKSPACE/JavaPrograming/src/Revisao/Pessoa.java
@@ -0,0 +1,21 @@
+package Revisao;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    double altura;
+    String genero;
+
+    public void falar(){
+        System.out.println("Estou falando");
+    }
+
+    public void andar(){
+        System.out.println("Estou andando");
+    }
+
+    public void dormir(){
+        System.out.println("dormir");
+    }
+
+}
