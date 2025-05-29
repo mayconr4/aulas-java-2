3b4310a2d03c1662c3a9a89f8927059f139586f6
mayconr4 <mayconhs2007@gmail.com>
2025-05-29 16:50:32 -0300

    Add files via upload

---
+++ WORKSPACE/JavaPrograming/src/Revisao/CalculoCientifico.java
@@ -0,0 +1,11 @@
+package Revisao;
+
+public class CalculoCientifico  extends Calculo{
+    Calculo valor_01;
+
+    public  double raizQuadrada(double a ){
+
+        double raiz = Math.sqrt(a);
+        return raiz;
+    }
+}
