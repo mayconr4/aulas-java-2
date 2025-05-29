4fad05631134bfd0a0c64383d7c8f4d28b39ef9f
mayconr4 <mayconhs2007@gmail.com>
2025-05-29 16:43:30 -0300

    em andamento calculo

---
+++ WORKSPACE/JavaPrograming/src/Revisao/Calculo.java
@@ -0,0 +1,27 @@
+package Revisao;
+
+public class Calculo {
+    private double valor_01;
+    private double valor_02;
+    private double total;
+
+
+    public double getValor_02() {
+        return valor_02;
+    }
+
+    public void setValor_02(double valor_02) {
+        this.valor_02 = valor_02;
+    }
+
+    public double getValor_01() {
+        return valor_01;
+    }
+
+    public void setValor_01(double valor_01) {
+        this.valor_01 = valor_01;
+    }
+
+
+
+}
