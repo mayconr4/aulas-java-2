c3d4a78da72c26200cad58bf545656eebad29a8e
mayconr4 <mayconhs2007@gmail.com>
2025-06-03 16:10:38 -0300

    Add files via upload

---
+++ WORKSPACE/JavaPrograming/src/AnimalMarinho/Aquario.java
@@ -0,0 +1,10 @@
+package AnimalMarinho;
+
+public class Aquario {
+    public static void main(String[] args) {
+        Peixe peixe = new Peixe();
+
+        peixe.nadar("sim");
+    }
+
+}
---
+++ WORKSPACE/JavaPrograming/src/AnimalMarinho/Peixe.java
@@ -0,0 +1,15 @@
+package AnimalMarinho;
+
+import ClassesAbstratas.Animal;
+
+public class Peixe extends Animal {
+    private String nadando;
+
+    public  Peixe(){
+
+    }
+
+    public Peixe (String nadando){
+        this.nadando = nadando;
+    }
+}
