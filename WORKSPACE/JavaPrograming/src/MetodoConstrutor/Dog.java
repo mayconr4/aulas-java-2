b08175918ea90635a475d2cc22edaeb22fc6dda5
mayconr4 <mayconhs2007@gmail.com>
2025-06-03 16:10:21 -0300

    Add files via upload

---
+++ WORKSPACE/JavaPrograming/src/MetodoConstrutor/Dog.java
@@ -0,0 +1,16 @@
+package MetodoConstrutor;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        Animal dog =  new Animal("DIDI",1);
+
+        System.out.println(dog.getNome());
+        System.out.println(dog.getIdade());
+        String a = "Amora";
+        int idade = 1;
+
+
+    }
+
+}
