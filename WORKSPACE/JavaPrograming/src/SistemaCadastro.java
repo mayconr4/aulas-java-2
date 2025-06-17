87224474a3e707cacb85b75033b6b29ee08f57ba
mayconr4 <mayconhs2007@gmail.com>
2025-06-17 16:59:18 -0300

    em andamento

---
+++ WORKSPACE/JavaPrograming/src/SistemaCadastro.java
@@ -0,0 +1,24 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SistemaCadastro {
+/*Criar um sistema de cadastro de alunos que armazena nome e nota em um HashMap,
+ imprime a lista ordenada pela nota usando o Comparable, e permite buscar por nome */
+    public static void main(String[] args) {
+        HashMap<String,Double>aluno = new HashMap<>();
+        Scanner ler = new Scanner(System.in);
+        String nome, nota;
+
+        System.out.println("Digite o nome do aluno");
+        aluno.put(nome);
+
+
+
+
+
+
+    }
+
+}
